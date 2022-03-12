9e71e4c833342f1e96ed9351ce7f85c52f8c5696
Sesko <Sesko.DK@gmail.com>
2022-03-12 17:55:35 +0100

    clean structure

---
+++ image_gen/generate_grid_images.py
@@ -0,0 +1,36 @@
+from train_generator import utils
+import torch
+import torchvision
+from tqdm import tqdm
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+
+def generate_grid_images(model, n_images, folder):
+    utils.create_new_folder(folder)
+    for i in tqdm(range(n_images), desc="Generate new grid images"):
+        z = torch.randn(size=(20, 100, 1, 1), dtype=torch.float32)
+        images = model(z)
+        images = (0.5 * images + 0.5)
+        grid = torchvision.utils.make_grid(images, nrow=4)
+        grid = grid.permute(1, 2, 0).numpy()
+
+        plt.imsave(Path(folder, f"{i}.png"), grid)
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path_grid_images", help="path of the generated grid images", type=str, default="data/generated_grid_images")
+    parser.add_argument("--n_images", help="Number of generated images", type=int, default=10)
+    parser.add_argument("--model", help="path of the trained generator", type=str,
+                        default="trained_models/2022-03-08_15_49_59.pt")
+    args = parser.parse_args()
+
+    path_grid_images = Path(args.path_grid_images)
+    model_path = args.model
+    n_images = args.n_images
+
+    generator = utils.loadModel(model_path)
+
+    generate_grid_images(model=generator, folder=path_grid_images, n_images=n_images)
---
+++ train_generator/utils.py
@@ -0,0 +1,34 @@
+import torch
+import shutil
+import datetime
+import numpy as np
+from PIL import Image
+from pathlib import Path
+from model import cnn
+
+
+def create_new_folder(dir):
+    if dir.exists():
+        shutil.rmtree(dir)
+        dir.mkdir(parents=True, exist_ok=True)
+    else:
+        dir.mkdir(parents=True, exist_ok=True)
+
+
+def saveModel(model):
+    modelDir = Path('../trained_models')
+    modelDir.mkdir(parents=True, exist_ok=True)
+    modelName = '{:%Y-%m-%d_%H_%M_%S}.pt'.format(datetime.datetime.now())
+    torch.save(model.state_dict(), modelDir / modelName)
+
+
+def loadModel(modelPath: str):
+    model = cnn.CNNGenerator()
+    model.load_state_dict(torch.load(modelPath))
+    model.eval()
+    return model
+
+
+def get_shape(root_dir: str, idx: str):
+    path = "{root_dir}/{image}.png".format(root_dir=root_dir, image=f"{idx+1}".zfill(3))
+    return np.array(Image.open(path)).shape
